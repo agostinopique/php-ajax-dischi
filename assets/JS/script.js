8885371652efcaf176d63ad17c03995c112508fc
agostinopique <ago.pique@yahoo.it>
2022-06-09 21:58:15 +0200

    Completato bonus con filtro di ricerca incrociato;

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,7 +4,13 @@ const app = new Vue ({
     el: '#app',
     data: {
         apiURL: 'http://localhost:8888/PHP%20Projects/PHP%20Dischi/%20php-ajax-dischi/assets/database/api.php',
-        discArr: []
+        apiParams:{
+            genre: 'default',
+            author: 'default'
+        },
+        discArr: [],
+        genreArr: [],
+        artistArr: [],
     },
 
     mounted(){
@@ -13,11 +19,41 @@ const app = new Vue ({
 
     methods : {
         getApi(){
-            axios.get(this.apiURL)
+            axios.get(this.apiURL, {
+                params: this.apiParams,
+            }) 
             .then((res) => {
-                console.log(res.data);
                 this.discArr = res.data; 
+
+                this.discArr.forEach(album => {
+                    if(!this.genreArr.includes(album.genre)){
+                        this.genreArr.push(album.genre)
+                    }
+                });
+
+                this.discArr.forEach(album => {
+                    if(!this.artistArr.includes(album.author)){
+                        this.artistArr.push(album.author)
+                    }
+                })
+                console.log(this.artistArr);
+                console.log(this.genreArr);
+            })
+            .catch (err =>{
+                console.log('ERROR', err)
             });
-        } 
+        },
+
+        getSearchApi(){
+            axios.get(this.apiURL, {
+                params: this.apiParams,
+            })  
+            .then((res) => {
+                this.discArr = res.data;
+                // console.log(res);
+            })
+            console.log(this.apiParams.genre)
+            console.log(this.apiParams.author)
+        }
     }
 })
